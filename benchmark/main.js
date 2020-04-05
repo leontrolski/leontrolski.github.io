bf6b7ab3335ea84402f6f12adf3272b900a70df3
Oliver Russell <ojhrussell@gmail.com>
2020-04-05 12:43:37 +0100

    revert 1 performance

--- benchmark/main.js
+++ benchmark/main.js
@@ -80,27 +80,17 @@ m.update = (el, v)=>{
     for (const name of data) if (el.dataset[name] !== `${v.attrs[`data-${name}`]}`) el.dataset[name] = v.attrs[`data-${name}`]
     for (const name of Object.keys(el.dataset)) if (!data.includes(name)) delete el.dataset[name]
 }
-m.fresh = (el, v)=>{
-    if (!v.__m) return el.data = v
-    el.classList.add(...v.classes)
-    const normal = Object.keys(v.attrs).filter(name=>!name.startsWith('data-'))
-    for (const name of normal) el[name] = v.attrs[name]
-    const data = Object.keys(v.attrs).filter(name=>name.startsWith('data-')).map(name=>name.slice(5))
-    for (const name of data) el.dataset[name] = v.attrs[`data-${name}`]
-}
 m.makeEl = v=>v.__m? document.createElement(v.tag) : document.createTextNode(v)
 m.render = (parent, v)=>{
     const olds = parent.childNodes || []
     const news = v.children || []
     for (const _ of Array(Math.max(0, olds.length - news.length))) parent.removeChild(parent.lastChild)
     for (const [i, child] of news.entries()){
-        const original = olds[i]
-        let el = original || m.makeEl(child)
+        let el = olds[i] || m.makeEl(child)
         if (!olds[i]) parent.appendChild(el)
         const mismatch = (el.tagName || '') !== (child.tag || '').toUpperCase()
         if (mismatch) (el = m.makeEl(child)) && parent.replaceChild(el, olds[i])
-        if (!original || mismatch) m.fresh(el, child)
-        else m.update(el, child)
+        m.update(el, child)
         m.render(el, child)
     }
 }
