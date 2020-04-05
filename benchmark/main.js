4f9cea8a5afc55252d38eb1aa1a20eda264a880f
Oliver Russell <oli@bulb.co.uk>
2020-04-05 08:53:09 +0100

    maxify vanilla-js code

--- benchmark/main.js
+++ benchmark/main.js
@@ -1,3 +1,84 @@
-(function(){'use strict';function l(c){return'<td class="TableCell" data-text="'+c+'">'+c+"</td>"}function m(c){for(var a='<ul class="TreeNode">',d=0;d<c.children.length;d++)var b=c.children[d],a=a+(b.container?m(b):'<li class="TreeLeaf">'+b.id+"</li>");return a+"</ul>"}uibench.init("Vanilla[innerHTML]","1.0.0");function n(c){"TableCell"===c.target.className&&(console.log("Click",c.target.getAttribute("data-text")),c.preventDefault(),c.stopPropagation())}
-document.addEventListener("DOMContentLoaded",function(){var c=document.querySelector("#App");uibench.run(function(a){var d=a&&a.location,b='<div class="Main">';if("table"===d){for(var d=a.table.items,f='<table class="Table"><tbody>',g=0;g<d.length;g++){for(var e=d[g],h=e.props,k='<tr class="TableRow'+(e.active?" active":"")+'" data-id="'+e.id+'">',k=k+l("#"+e.id),e=0;e<h.length;e++)k+=l(h[e]);f+=k+"</tr>"}b+=f+"</tbody></table>"}else if("anim"===d){d=a.anim.items;f='<div class="Anim">';for(g=0;g<
-d.length;g++)h=d[g],f+='<div class="AnimBox" style="'+("border-radius:"+h.time%10+"px;"+("background:rgba(0,0,0,"+(.5+h.time%10/10)+")"))+'" data-id="'+h.id+'"></div>';b+=f+"</div>"}else"tree"===d&&(b+='<div class="Tree">'+m(a.tree.root)+"</div>");c.innerHTML=b+"</div>";if("table"===a.location)for(a=c.querySelectorAll(".TableCell"),b=0;b<a.length;b++)a[b].onclick=n},function(a){document.body.innerHTML="<pre>"+JSON.stringify(a,null," ")+"</pre>"})});}).call();
+function renderTableCell(props) {
+    return `<td class="TableCell" data-text="${props}">${props}</td>`;
+}
+function renderTableRow(data) {
+    const props = data.props;
+    const active = data.active ? " active" : "";
+    let result = `<tr class="TableRow${active}" data-id="${data.id}">`;
+    result += renderTableCell("#" + data.id);
+    for (let i = 0; i < props.length; i++) {
+        result += renderTableCell(props[i]);
+    }
+    return result + `</tr>`;
+}
+function renderTable(data) {
+    const items = data.items;
+    let result = `<table class="Table"><tbody>`;
+    for (let i = 0; i < items.length; i++) {
+        result += renderTableRow(items[i]);
+    }
+    return result + `</tbody></table>`;
+}
+function renderAnimBox(props) {
+    const style = `border-radius:${props.time % 10}px;` +
+        `background:rgba(0,0,0,${0.5 + ((props.time % 10) / 10)})`;
+    return `<div class="AnimBox" style="${style}" data-id="${props.id}"></div>`;
+}
+function renderAnim(props) {
+    const items = props.items;
+    let result = `<div class="Anim">`;
+    for (let i = 0; i < items.length; i++) {
+        result += renderAnimBox(items[i]);
+    }
+    return result + `</div>`;
+}
+function renderTreeLeaf(props) {
+    return `<li class="TreeLeaf">${props.id}</li>`;
+}
+function renderTreeNode(data) {
+    let result = `<ul class="TreeNode">`;
+    for (let i = 0; i < data.children.length; i++) {
+        const n = data.children[i];
+        result += n.container ? renderTreeNode(n) : renderTreeLeaf(n);
+    }
+    return result += `</ul>`;
+}
+function renderTree(props) {
+    return `<div class="Tree">${renderTreeNode(props.root)}</div>`;
+}
+function renderMain(data) {
+    const location = data && data.location;
+    let result = `<div class="Main">`;
+    if (location === "table") {
+        result += renderTable(data.table);
+    }
+    else if (location === "anim") {
+        result += renderAnim(data.anim);
+    }
+    else if (location === "tree") {
+        result += renderTree(data.tree);
+    }
+    return result + `</div>`;
+}
+uibench.init("Vanilla[innerHTML]", "1.0.0");
+function handleClick(e) {
+    if (e.target.className === "TableCell") {
+        console.log("Click", e.target.getAttribute("data-text"));
+        e.preventDefault();
+        e.stopPropagation();
+    }
+}
+document.addEventListener("DOMContentLoaded", (e) => {
+    const container = document.querySelector("#App");
+    uibench.run((state) => {
+        container.innerHTML = renderMain(state);
+        if (state.location === "table") {
+            const cells = container.querySelectorAll(".TableCell");
+            for (let i = 0; i < cells.length; i++) {
+                cells[i].onclick = handleClick;
+            }
+        }
+    }, (samples) => {
+        document.body.innerHTML = "<pre>" + JSON.stringify(samples, null, " ") + "</pre>";
+    });
+});
