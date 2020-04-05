d1593b2e186b97b9c2d4c9dea93ca66e3fdbb76e
Oliver Russell <ojhrussell@gmail.com>
2020-04-05 10:07:23 +0100

    convert to framework

--- benchmark/main.js
+++ benchmark/main.js
@@ -1,84 +1,83 @@
-function renderTableCell(props) {
-    return `<td class="TableCell" data-text="${props}">${props}</td>`;
+// UI
+function tableCellClick(e, prop){
+    console.log(`Click${prop}`)
+    e.preventDefault()
+    e.stopPropagation()
 }
-function renderTableRow(data) {
-    const props = data.props;
-    const active = data.active ? " active" : "";
-    let result = `<tr class="TableRow${active}" data-id="${data.id}">`;
-    result += renderTableCell("#" + data.id);
-    for (let i = 0; i < props.length; i++) {
-        result += renderTableCell(props[i]);
+const renderTableCell = prop=>m('td.TableCell',
+    {'data-text': prop, onclick: e=>tableCellClick(e, prop)},
+    prop,
+)
+const renderTableRow = data=>m('tr.TableRow',
+    {class: data.active? ['active'] : [], 'data-id': data.id},
+    renderTableCell(`#${data.id}`),
+    data.props.map(renderTableCell),
+)
+const renderTable = data=>m('table.Table', m('tbody', data.table.items.map(renderTableRow)))
+const animBoxStyle = time=>`
+    border-radius:${time % 10}px;
+    background:rgba(0,0,0,${0.5 + ((time % 10) / 10)});
+`
+const renderAnimBox = props=>m('.AnimBox', {style: animBoxStyle(props.time), 'data-id': props.id})
+const renderAnim =data=>m('.Anim', data.anim.items.map(renderAnimBox))
+const renderTreeLeaf = props=>m('li.TreeLeaf', props.id)
+const renderTreeNode = data=>m('ul.TreeNode', data.children.map(n=>n.container? renderTreeNode(n) : renderTreeLeaf(n)))
+const renderTree = data=>m('.Tree', renderTreeNode(data.tree.root))
+
+// test
+uibench.init("React33", "0.0.1")
+function renderState(container, state){
+    const location = state && state.location
+    const fs = {
+        table: renderTable,
+        anim: renderAnim,
+        tree: renderTree,
     }
-    return result + `</tr>`;
+    m.render(container, {children: [m('.Main', fs[location](state))]})
 }
-function renderTable(data) {
-    const items = data.items;
-    let result = `<table class="Table"><tbody>`;
-    for (let i = 0; i < items.length; i++) {
-        result += renderTableRow(items[i]);
-    }
-    return result + `</tbody></table>`;
-}
-function renderAnimBox(props) {
-    const style = `border-radius:${props.time % 10}px;` +
-        `background:rgba(0,0,0,${0.5 + ((props.time % 10) / 10)})`;
-    return `<div class="AnimBox" style="${style}" data-id="${props.id}"></div>`;
-}
-function renderAnim(props) {
-    const items = props.items;
-    let result = `<div class="Anim">`;
-    for (let i = 0; i < items.length; i++) {
-        result += renderAnimBox(items[i]);
-    }
-    return result + `</div>`;
-}
-function renderTreeLeaf(props) {
-    return `<li class="TreeLeaf">${props.id}</li>`;
-}
-function renderTreeNode(data) {
-    let result = `<ul class="TreeNode">`;
-    for (let i = 0; i < data.children.length; i++) {
-        const n = data.children[i];
-        result += n.container ? renderTreeNode(n) : renderTreeLeaf(n);
-    }
-    return result += `</ul>`;
-}
-function renderTree(props) {
-    return `<div class="Tree">${renderTreeNode(props.root)}</div>`;
-}
-function renderMain(data) {
-    const location = data && data.location;
-    let result = `<div class="Main">`;
-    if (location === "table") {
-        result += renderTable(data.table);
-    }
-    else if (location === "anim") {
-        result += renderAnim(data.anim);
-    }
-    else if (location === "tree") {
-        result += renderTree(data.tree);
-    }
-    return result + `</div>`;
-}
-uibench.init("Vanilla[innerHTML]", "1.0.0");
-function handleClick(e) {
-    if (e.target.className === "TableCell") {
-        console.log("Click", e.target.getAttribute("data-text"));
-        e.preventDefault();
-        e.stopPropagation();
-    }
+function renderSamples(samples){
+    document.body.innerHTML = "<pre>" + JSON.stringify(samples, null, " ") + "</pre>"
 }
 document.addEventListener("DOMContentLoaded", (e) => {
-    const container = document.querySelector("#App");
-    uibench.run((state) => {
-        container.innerHTML = renderMain(state);
-        if (state.location === "table") {
-            const cells = container.querySelectorAll(".TableCell");
-            for (let i = 0; i < cells.length; i++) {
-                cells[i].onclick = handleClick;
-            }
-        }
-    }, (samples) => {
-        document.body.innerHTML = "<pre>" + JSON.stringify(samples, null, " ") + "</pre>";
-    });
-});
+    const container = document.querySelector("#App")
+    uibench.run(state=>renderState(container, state), renderSamples)
+})
+
+// Framework:
+const m = (...args)=>{
+    let [attrs, [head, ...tail]] = [{}, args]
+    let [tag, ...classes] = head.split('.')
+    if (tail.length && !m.isRenderable(tail[0])) [attrs, ...tail] = tail
+    if (attrs.class) classes = [...classes, ...attrs.class]
+    attrs = {...attrs}; delete attrs.class
+    const children = []
+    const addChildren = v=>v === null? null : Array.isArray(v)? v.map(addChildren) : children.push(v)
+    addChildren(tail)
+    return {__m: true, tag: tag || 'div', attrs, classes, children}
+}
+m.isRenderable = v =>v === null || ['string', 'number'].includes(typeof v) || v.__m || Array.isArray(v)
+m.update = (el, v)=>{
+    if (!v.__m) return el.data === `${v}` || (el.data = v)
+    for (const name of v.classes) if (!el.classList.contains(name)) el.classList.add(name)
+    for (const name of el.classList) if (!v.classes.includes(name)) el.classList.remove(name)
+    const normal = Object.keys(v.attrs).filter(name=>!name.startsWith('data-'))
+    for (const name of normal) if (el[name] !== v.attrs[name]) el[name] = v.attrs[name]
+    for (const {name} of el.attributes) if (!normal.includes(name) && name !== 'class') el.removeAttribute(name)
+    const data = Object.keys(v.attrs).filter(name=>name.startsWith('data-'))
+    for (const name of data) if (el.dataset[name.slice(5)] !== `${v.attrs[name]}`) el.dataset[name.slice(5)] = v.attrs[name]
+    for (const name of Object.keys(el.dataset)) if (!data.includes(`data-${name}`)) delete el.dataset[name.slice(5)]
+}
+m.makeEl = v=>v.__m? document.createElement(v.tag) : document.createTextNode(v)
+m.render = (parent, v)=>{
+    const olds = parent.childNodes || []
+    const news = v.children || []
+    for (const _ of Array(Math.max(0, olds.length - news.length))) parent.removeChild(parent.lastChild)
+    for (const [i, child] of news.entries()){
+        let el = olds[i] || m.makeEl(child)
+        if (!olds[i]) parent.appendChild(el)
+        const mismatch = (el.tagName || '') !== (child.tag || '').toUpperCase()
+        if (mismatch) (el = m.makeEl(child)) && parent.replaceChild(el, olds[i])
+        m.update(el, child)
+        m.render(el, child)
+    }
+}
